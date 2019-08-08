78c9c9b2eb6bb7e2dbf77e3b05cbd89d2ece3366
mwconceptx <45110217+M1kePL@users.noreply.github.com>
2019-08-08 18:20:23 +0200

    added js for scroll

--- app.js
+++ app.js
@@ -4,10 +4,36 @@ document.addEventListener('DOMContentLoaded', function () {
     const root = document.querySelector('.root');
     const sections = document.querySelectorAll('.section');
 
+    let currentIndex = 0;
+    let isActive = false;
+
     document.addEventListener('mousewheel', function (e) {
-        console.log(e.wheelDelta);
+
+        if (isActive) return;
+        isActive = true;
+
+        setTimeout(function () {
+            isActive = false;
+        }, 500);
+
+        const direction = e.wheelDelta < 0 ? 1 : -1;
+
+        if (direction === 1) {
+            currentIndex++;
+            currentIndex > sections.length - 1 ? currentIndex = sections.length - 1 : false;
+
+        } else {
+            currentIndex--;
+            currentIndex < 0 ? currentIndex = 0 : false;
+        }
+        console.log(direction);
+        console.log(currentIndex);
+        console.log(sections[currentIndex]);
+
+        sections[currentIndex].scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        });
 
     })
-
-
 })
